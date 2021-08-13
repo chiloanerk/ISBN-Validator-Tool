e7ee2e49e99e4b3c5eb9f9512f6beb56124205f6
Relebogile Chiloane <chiloane.rk@live.com>
2021-08-13 16:31:34 +0200

    Add project files.

---
+++ ISBN Validator/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.mtbISBN = New System.Windows.Forms.MaskedTextBox()
+        Me.btnCompute = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ISBN number:"
+        '
+        'mtbISBN
+        '
+        Me.mtbISBN.Location = New System.Drawing.Point(146, 12)
+        Me.mtbISBN.Mask = "0-00-000000-&"
+        Me.mtbISBN.Name = "mtbISBN"
+        Me.mtbISBN.Size = New System.Drawing.Size(100, 23)
+        Me.mtbISBN.TabIndex = 1
+        '
+        'btnCompute
+        '
+        Me.btnCompute.Location = New System.Drawing.Point(85, 41)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(136, 32)
+        Me.btnCompute.TabIndex = 2
+        Me.btnCompute.Text = "Check Validity"
+        Me.btnCompute.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(84, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Valid ISBN:"
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(153, 79)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.Size = New System.Drawing.Size(69, 23)
+        Me.txtOutput.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 111)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.mtbISBN)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "ISBN"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents mtbISBN As MaskedTextBox
+    Friend WithEvents btnCompute As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtOutput As TextBox
+End Class
---
+++ ISBN Validator/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+  Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+    Dim ISBN(9) As Integer
+    Dim input As String = mtbISBN.Text
+    Dim sum As Integer
+    Dim multiplier As Integer = 10
+    Dim index As Integer = 0
+
+    'Check if the last digit is a number or X
+    If input.EndsWith("X".ToUpper()) Then
+      ISBN(9) = 10
+      sum += 10
+    ElseIf IsNumeric(input.Substring(12, 1)) Then
+      ISBN(9) = CInt(input.Substring(12, 1))
+      sum += ISBN(9)
+    Else
+      MessageBox.Show("You entered invalid charachter!", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+      Exit Sub
+    End If
+
+    'Create the loop for the input ISBN including the hyphens and exclude them in the array
+    For i As Integer = 0 To input.Length - 2
+      If IsNumeric(input.Substring(i, 1)) Then
+        ISBN(index) = CInt(input.Substring(i, 1))
+        sum += multiplier * ISBN(index)
+        multiplier -= 1
+        index += 1
+      End If
+    Next
+    'Check if divisible by 10 and print an output
+    If sum Mod 11 = 0 Then
+      txtOutput.Text = "YES"
+    Else
+      txtOutput.Text = "NO"
+    End If
+
+  End Sub
+End Class
